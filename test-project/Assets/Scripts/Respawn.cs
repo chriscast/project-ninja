3084ab65a97b6b24f51782d913db6a287aa7dbf5
yoshiyoshi <katsuhi.sugarfang@gmail.com>
2014-03-27 22:47:40 +0900

    プレイヤーが落下した際に原点付近に戻すスクリプトを追加

---
+++ test-project/Assets/Scripts/Respawn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.localPosition.y < -10f)
+		{
+			transform.localPosition = new Vector3(0f, 10f, 0f);
+		}
+	}
+}
