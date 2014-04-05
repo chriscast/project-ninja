94886b139aa4b54e862731fdc75b9d8233597a4c
yohei-washizaki <yohei.washizaki@gmail.com>
2014-04-06 02:53:08 +0900

    壁を登れるようになった。

---
+++ test-project/Assets/hitcheck.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hitcheck : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("OnTriggerEnter with" + other);
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		Debug.Log("OnCollisionEnter with" + other);
+	}
+}
